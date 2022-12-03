3dd785793c9ba06cf6abd62867002d46bf29a542
Youthan Irigoyen <a01750476@itesm.mx>
2022-12-03 14:29:13 -0600

    Added corrections

--- scripts/tasks/translator_comparator.py
+++ scripts/tasks/translator_comparator.py
@@ -2,6 +2,7 @@ import os
 import deepl
 import numpy as np
 import logging as log
+from abc import abstractmethod
 from pathlib import Path
 from dotenv import load_dotenv
 from google.cloud import translate_v2 as translate
@@ -11,70 +12,99 @@ from nltk.translate.bleu_score import sentence_bleu
 load_dotenv()
 
 
-def get_lines(filepath: str | Path, limit: int) -> list[str]:
-    log.debug(f"Getting first {limit} text lines from {filepath}")
-    with open(filepath, 'r') as f:
-        file_lines = []
-        for index, line in enumerate(f):
-            file_lines.append(line)
-            if index + 1 == limit:
-                break
-    log.debug(f"Finished retrieving text lines. Got {len(file_lines)} lines\n")
-    return file_lines
+class Translator:
+    def __init__(self, data_path: Path) -> None:
+        self._data_path = data_path
+        self._lines_to_translate = self.get_lines(data_path.joinpath("ES-EN.txt"))
+        self._reference_lines = self.get_lines(data_path.joinpath("EN-ES.txt"))
+
+    @staticmethod
+    def get_lines(filepath: Path, limit: int = -1) -> list[str]:
+        log.debug(f"Getting first {limit} text lines from {filepath}")
+        with open(filepath, 'r') as f:
+            file_lines = []
+            for index, line in enumerate(f):
+                file_lines.append(line)
+                if index + 1 == limit:
+                    log.debug(f"Finished retrieving text lines. Got {len(file_lines)} lines\n")
+                    break
+            else:
+                log.debug(f"Finished retrieving text lines. Got {len(file_lines)} lines\n")
+        return file_lines
+
+    def get_avg_bleu(self, lines) -> float | None:
+        if len(lines) > 0:
+            log.debug("Getting average bleu score")
+            bleu_scores = []
+            for ref, res in zip(self._reference_lines, lines):
+                bleu_scores.append(sentence_bleu([ref], res))
+            result = np.average(np.asarray(bleu_scores))
+            log.debug(f"Bleu score: {result}")
+            return result
+        else:
+            log.warn("Cannot get average. Not enough result lines")
+            return None
+    
+    @staticmethod
+    def _write_translations(filepath: Path, lines: list[str]) -> None:
+        log.debug(f"Writing translation to {filepath}")
+        with open(filepath, 'w') as f:
+            f.writelines(lines)
+    
+    @abstractmethod
+    def translate(self, write_to_file: bool = False) -> list[str]:
+        pass
 
 
-def get_avg_bleu(ref_lines: list[str], res_lines: list[str]) -> float:
-    bleu_scores = []
-    log.debug("Getting average bleu score")
-    for ref_line, res_line in zip(ref_lines, res_lines):
-        bleu_scores.append(sentence_bleu([ref_line], res_line))
-    avg = np.average(np.asarray(bleu_scores))
-    log.debug(f"Got score of {avg}")
-    return avg
+class GoogleCloudTranslator(Translator):
+    def __init__(self, data_path: Path, target_language: str = "es") -> None:
+        super().__init__(data_path)
+        # Set Google Cloud client
+        self._google_api_path = Path("./cloudapikey.json")
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(self._google_api_path)
+        self._client = translate.Client()
+        # Set target language
+        self._target_language = target_language
+        # Set writing path
+        self._google_cloud_filepath = Path("./scripts/testing/googleCloudTranslation.txt")
+
+    def translate(self, write_to_file: bool = False) -> list[str]:
+        log.debug("Translating lines with Google Cloud...")
+        lines = [self._client.translate(line, target_language=self._target_language)["translatedText"] + "\n" for line in self._lines_to_translate]
+        if write_to_file:
+            self._write_translations(self._google_cloud_filepath, lines)
+        return lines
 
 
-def get_cloud_lines(client, lines: list[str], target_language: str = "es") -> list[str]:
-    translations = []
-    for line in lines:
-        translations.append(client.translate(line, target_language=target_language)["translatedText"] + "\n")
-    return translations
+class DeepLTranslator(Translator):
+    def __init__(self, data_path: Path, target_language: str = "es") -> None:
+        super().__init__(data_path)
+        # Set DeepL client
+        self._deepl_api_key = os.getenv('DEEPL_API_KEY')
+        self._client = deepl.Translator(self._deepl_api_key)
+        # Set target language
+        self._target_language = target_language
+        # Set writing path
+        self._deepl_filepath = Path("./scripts/testing/deepLTranslation.txt")
+
+    def translate(self, write_to_file: bool = False) -> list[str]:
+        log.debug("Translating lines with Google Cloud...")
+        lines = [self._client.translate_text(line, target_lang=self._target_language).text for line in self._lines_to_translate] 
+        if write_to_file:
+            self._write_translations(self._deepl_filepath, lines)
+        return lines
 
 
-def get_deepl_lines(client: deepl.translator.Translator, lines: list[str], target_language: str = "es") -> list[str]:
-    translations = []
-    for line in lines:
-        translations.append(client.translate_text(line, target_lang=target_language).text)
-    return translations
-
-
-def test_comparison(limit: int = 100) -> None:
+def test_comparison() -> None:
     # Set paths
     data_path = Path("./data")
-    source_path = data_path.joinpath("ES-EN.txt")
-    reference_path = data_path.joinpath("EN-ES.txt")
-    # Set clients from both APIs
-    google_api_path = Path("./cloudapikey.json")
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(google_api_path)
-    cloud_client = translate.Client()
-    deepl_api_key = os.getenv('DEEPL_API_KEY')
-    deepl_client = deepl.Translator(deepl_api_key)
-    # Get lines from text
-    lines_to_translate = get_lines(source_path, limit)
-    actual_translated_lines = get_lines(reference_path, limit)
-    # Translate lines
-    cloud_translated_lines = get_cloud_lines(cloud_client, lines_to_translate)
-    deepl_translated_lines = get_deepl_lines(deepl_client, lines_to_translate)
-    # Write translations to files
-    google_cloud_file = Path("./scripts/testing/googleCloudTranslation.txt")
-    deepl_file = Path("./scripts/testing/deepLTranslation.txt")
-    log.debug(f"Writing Google Cloud translation to {google_cloud_file}")
-    log.debug(f"Writing DeepL translation to {deepl_file}\n")
-    with open(google_cloud_file, 'w') as f:
-        f.writelines(cloud_translated_lines)
-    with open(deepl_file, 'w') as f:
-        f.writelines(deepl_translated_lines)
-    # Compare translations
-    cloud_bleu_score = get_avg_bleu(actual_translated_lines, cloud_translated_lines)
-    deepl_bleu_score = get_avg_bleu(actual_translated_lines, deepl_translated_lines)
-    print(f"Google Cloud BLEU score: {cloud_bleu_score}")
-    print(f"DeepL SE BLEU score: {deepl_bleu_score}")
+    # Create translator instances
+    google_cloud_translator = GoogleCloudTranslator(data_path)
+    deepl_translator = DeepLTranslator(data_path)
+    # Get translations and bleu scores
+    google_cloud_translations = google_cloud_translator.translate(write_to_file=True)
+    google_cloud_bleu = google_cloud_translator.get_avg_bleu(google_cloud_translations)
+    print(f"Google Cloud BLEU score: {google_cloud_bleu}")
+    deepl_translations = deepl_translator.translate(write_to_file=True)
+    deepl_bleu = deepl_translator.get_avg_bleu(deepl_translations)
+    print(f"DeepL SE BLEU score: {deepl_bleu}")
